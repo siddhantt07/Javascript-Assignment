1b7b3f379777ca80650d703403e7f31aa994b40b
siddhantt07 <78294508+siddhantt07@users.noreply.github.com>
2021-02-02 14:32:42 +0530

    Update task.js

--- task.js
+++ task.js
@@ -7,26 +7,4 @@ function myFunc(){
     var msg="First Name: "+val+"<br>Last Name: "+val1+"<br>Email: "+val2+"<br>Contact No.: "+val3;
     document.getElementById("content").innerHTML="Your details have been saved<br>"+msg;
 }
-/* Ajax
-document.getElementById("button").addEventListener("click",function(){
-    var xhttp;
-
-    if (window.XMLHttpRequest)
-    {// If the browser if IE7+[or]Firefox[or]Chrome[or]Opera[or]Safari
-        xhttp=new XMLHttpRequest();
-    }
-    else
-    {//If browser is IE6, IE5
-        xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    //var xhttp= new XMLHttpRequest();
-    xhttp.onreadystatechange=function(){
-        if(xhttp.readyState==4 && xhttp.status==200){
-            document.getElementById("content").innerHTML=xhttp.responseText;
-        }
-    }
-    xhttp.open("GET","ajax.txt",true);
-    xhttp.send();
-});*/
-   
 
