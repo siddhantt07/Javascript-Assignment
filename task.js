7cf85694e2c02cbcaff3372c1fbc2feab36b0d0b
siddhantt07 <78294508+siddhantt07@users.noreply.github.com>
2021-02-02 14:27:15 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-/*function myFunc(){
+function myFunc(){
     document.getElementById("content").innerHTML="Your details have been saved";
     var val=document.getElementById("form").elements[0].value;
     var val1=document.getElementById("form").elements[1].value;
@@ -6,7 +6,8 @@
     var val3=document.getElementById("form").elements[3].value;
     var msg="First Name: "+val+"<br>Last Name: "+val1+"<br>Email: "+val2+"<br>Contact No.: "+val3;
     document.getElementById("content").innerHTML="Your details have been saved<br>"+msg;
-}*/
+}
+/* Ajax
 document.getElementById("button").addEventListener("click",function(){
     var xhttp;
 
@@ -26,6 +27,6 @@ document.getElementById("button").addEventListener("click",function(){
     }
     xhttp.open("GET","ajax.txt",true);
     xhttp.send();
-});
+});*/
    
 
